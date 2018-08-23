ee2a4959933dd70f0949a63319d48a4f46a6267d
Alex Katz <aik49@cornell.edu>
2018-08-23 11:42:35 -0400

    Tweaked fib coding question

--- fib.cpp
+++ fib.cpp
@@ -1,7 +1,7 @@
 /* Feel free to include any libraries you need */
 
 /**
- * 1. Use variadic templates to calculate the Nth Fibonacci number.
+ * 1. Use variadic templates to calculate the Nth Fibonacci number at compile time.
  *
  * HINT: Recall that F(n) = F(n-1) + F(n-2)
  */
